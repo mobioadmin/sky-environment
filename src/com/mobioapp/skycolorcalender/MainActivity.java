6b6376bdcd3bde68531be1d3c788293136232217
sajal <sajal@sajal-pc>
2014-04-12 18:17:53 +0600

    rgb value from camera

--- src/com/mobioapp/skycolorcalender/MainActivity.java
+++ src/com/mobioapp/skycolorcalender/MainActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.mobioapp.skycolor.R;
@@ -20,7 +19,6 @@ public class MainActivity extends Activity {
 
 	private Camera cameraObject;
 	private ShowCamera showCamera;
-	private ImageView pic;
 
 	public static Camera isCameraAvailiable() {
 		Camera object = null;
@@ -35,7 +33,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		pic = (ImageView) findViewById(R.id.imageView1);
 		cameraObject = isCameraAvailiable();
 		showCamera = new ShowCamera(this, cameraObject);
 		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
@@ -44,6 +41,7 @@ public class MainActivity extends Activity {
 
 	public void snapIt(View view) {
 		cameraObject.takePicture(null, null, capturedIt);
+
 	}
 
 	private PictureCallback capturedIt = new PictureCallback() {
@@ -53,11 +51,9 @@ public class MainActivity extends Activity {
 
 			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 			
-			getColor(bitmap);
+			getAverageColor(bitmap);
 			
-//			Buffer imageBuffer;
-//			
-//			bitmap.copyPixelsToBuffer(imageBuffer);
+
 			if (bitmap == null) {
 				Toast.makeText(getApplicationContext(), "not taken",
 						Toast.LENGTH_SHORT).show();
@@ -65,49 +61,11 @@ public class MainActivity extends Activity {
 				Toast.makeText(getApplicationContext(), "taken",
 						Toast.LENGTH_SHORT).show();
 			}
-			cameraObject.release();
+//			cameraObject.release();
+			
+			
 		}
 	};
-	
-	
-	private void getColor(Bitmap bmp){
-		
-		long redBucket = 0;
-		long greenBucket = 0;
-		long blueBucket = 0;
-		long pixelCount = 0;
-	  bmp = bmp.copy(Bitmap.Config.ARGB_8888, true);  
-	  Log.i("arrayLength",""+bmp.getWidth()*bmp.getHeight());
-	    int intArray[] = new int[bmp.getWidth()*bmp.getHeight()];  
-	    bmp.getPixels(intArray, 0, bmp.getWidth(), 0, 0, bmp.getWidth(), bmp.getHeight());
-	    
-	    
-	   
-	    
-		for (int y = 0; y < intArray.length; y++)
-		{
-//		       
-//		        pixelCount++;
-//		        redBucket += Color.red(intArray[y]);
-//		        greenBucket += Color.green(intArray[y]);
-//		        blueBucket += Color.blue(intArray[y]);
-//		        // does alpha matter?
-//		   
-		}
-//		Log.i("red",""+redBucket/pixelCount);
-//		Log.i("green",""+greenBucket/pixelCount);
-//		Log.i("blue",""+blueBucket/pixelCount);
-		
-		
-//
-//		int averageColor = Color.rgb((int)(redBucket / pixelCount),
-//				(int) (greenBucket / pixelCount),
-//				(int) (blueBucket / pixelCount));
-//		
-//	Log.i("avgColor",""+averageColor);
-		
-		
-	}
 	
 	
 	private void getAverageColor(Bitmap bitmap){
@@ -120,20 +78,24 @@ public class MainActivity extends Activity {
 		{
 		    for (int x = 0; x < bitmap.getWidth(); x++)
 		    {
-		        Color c;// =bitmap.getPixel(x, y);
+		        int c =bitmap.getPixel(x, y);
 
 		       
 		        pixelCount++;
-//		        redBucket += Color.red(c);
-//		        greenBucket += Color.green(c);
-//		        blueBucket += Color.blue(c);
+		        redBucket += Color.red(c);
+		        greenBucket += Color.green(c);
+		        blueBucket += Color.blue(c);
 		        // does alpha matter?
 		    }
 		}
 
-//		Color averageColor = Color.rgb(redBucket / pixelCount,
-//		                                greenBucket / pixelCount,
-//		                                blueBucket / pixelCount);
+		int averageColor = Color.rgb(((int)(redBucket / pixelCount)),
+				((int)(greenBucket / pixelCount)),
+				((int)(blueBucket / pixelCount)));
+		
+//		Color.
+		
+		Log.i("avgColor",""+averageColor+":"+(int)(redBucket / pixelCount)+":"+(int)(greenBucket / pixelCount)+":"+(int)(blueBucket / pixelCount));
 //		
 		
 	}
